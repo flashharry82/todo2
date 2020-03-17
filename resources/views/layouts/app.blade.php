03f8f8dd4999aaeb06a028d41cd673828c461d63
Alan Stirling <flashharry82@yahoo.com>
2020-03-17 15:45:44 +0000

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple CRUD</title>
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="container">
+        @yield('main')
+    </div>
+</body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('main')
+<div class="row">
+<div class="col-sm-12">
+    <h1 class="display-3">Tasks</h1>
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td></td>
+          <td></td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($tasks as $task)
+        <tr>
+            <td>{{$task->id}}</td>
+            <td>{{$task->name}}</td>
+            <td>
+                <a href="{{ route('tasks.edit',$task->id)}}" class="btn btn-primary">Edit</a>
+            </td>
+            <td>
+                <form action="{{ route('tasks.destroy', $task->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+</div>
+@endsection
